d3fbe17ccce6ce63fd884f026201ef61e8eb2816
Aravind-91 <148511919+Aravind-91@users.noreply.github.com>
2026-02-12 14:47:32 +0530

    model trainer added

--- us_visa/entity/estimator.py
+++ us_visa/entity/estimator.py
@@ -18,3 +18,39 @@ class TargetValueMapping:
     def reverse_mapping(self):
         mapping_response = self._asdict()
         return dict(zip(mapping_response.values(), mapping_response.keys()))
+
+
+class UsvisaModel:
+    def __init__(self, preprocessing_object: Pipeline, trained_model_object: object):
+        """
+        preprocessing_object : Input object of preprocessor
+        trained_model_object : Input object of trained model
+        """
+        self.preprocessing_object = preprocessing_object
+        self.trained_model_object = trained_model_object
+
+    def predict(self, dataframe: DataFrame) -> DataFrame:
+        """
+        Function accepts raw inputs and then transform the raw inputs using preprocessing object
+        which guarantees that the inputs are in the same format as the training data
+        At last it performs prediction on transformed features.
+        """
+        logging.info("Entered predict method of UsvisaModel class")
+
+        try:
+            logging.info("Using trained model to get predictions")
+
+            transformed_feature = self.preprocessing_object.transform(dataframe)
+
+            logging.info("Used the trained model to get predictions")
+
+            return self.trained_model_object.predict(transformed_feature)
+
+        except Exception as e:
+            raise USvisaException(e, sys)
+
+    def __repr__(self):
+        return f"{type(self.trained_model_object).__name__}()"
+
+    def __str__(self):
+        return f"{type(self.trained_model_object).__name__}()"
--- us_visa/pipline/training_pipeline.py
+++ us_visa/pipline/training_pipeline.py
@@ -4,15 +4,18 @@ from us_visa.exception import USvisaException
 from us_visa.components.data_ingestion import DataIngestion
 from us_visa.components.data_validation import DataValidation
 from us_visa.components.data_transformation import DataTransformation
+from us_visa.components.model_trainer import ModelTrainer
 from us_visa.entity.config_entity import (
     DataIngestionConfig,
     DataValidationConfig,
     DataTransformationConfig,
+    ModelTrainerConfig,
 )
 from us_visa.entity.artifact_entity import (
     DataIngestionArtifact,
     DataValidationArtifact,
     DataTransformationArtifact,
+    ModelTrainerArtifact,
 )
 
 
@@ -21,6 +24,7 @@ class TrainPipeline:
         self.data_ingestion_config = DataIngestionConfig()
         self.data_validation_config = DataValidationConfig()
         self.data_transformation_config = DataTransformationConfig()
+        self.model_trainer_config = ModelTrainerConfig()
 
     def start_data_ingestion(self) -> DataIngestionArtifact:
         """
@@ -86,6 +90,22 @@ class TrainPipeline:
         except Exception as e:
             raise USvisaException(e, sys)
 
+    def start_model_trainer(
+        self, data_transformation_artifact: DataTransformationArtifact
+    ) -> ModelTrainerArtifact:
+        """
+        This Method is responsible for starting Model Training
+        """
+        try:
+            model_trainer = ModelTrainer(
+                data_transformation_artifact=data_transformation_artifact,
+                model_trainer_config=self.model_trainer_config,
+            )
+            model_trainer_artifact = model_trainer.initiate_model_trainer()
+            return model_trainer_artifact
+        except Exception as e:
+            raise USvisaException(e, sys)
+
     def run_pipeline(self) -> None:
         """
         This Method id responsible for running total pipeline
@@ -97,6 +117,9 @@ class TrainPipeline:
             )
             data_transformation_artifact = self.start_data_transformation(
                 data_ingestion_artifact, data_validation_artifact
+            )
+            model_trainer_artifact = self.start_model_trainer(
+                data_transformation_artifact=data_transformation_artifact
             )
         except Exception as e:
             raise USvisaException(e, sys)
